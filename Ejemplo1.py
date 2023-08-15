689d1bf8eeb29f0eeff2ce6bfc082fa67c5aa115
309 <309@academicos.cesde.edu.co>
2023-08-15 08:41:00 -0500

    feat:se reciben datos por teclado y se castean

--- Ejemplo1.py
+++ Ejemplo1.py
@@ -10,3 +10,13 @@ comidasFavoritas=["frisoles","chicharron","mazamorra","carneMolida"]
 
 print(comidasFavoritas)
 print(comidasFavoritas[2])
+
+#Entradas por teclado:
+LugarTrabajoUsuario=input("Digita el lugar de trabajo:")
+print(f"usted trabaja en: {LugarTrabajoUsuario}")
+
+ #PEDIMOS DOS NUMEROS
+nunmeroUno=int(input("Digita un numero : "))
+nunmeroDos=int(input("Digita otro numero : "))
+sumatoria=nunmeroUno+nunmeroDos
+print(f"la suma de {nunmeroUno}+{nunmeroDos}={sumatoria}")
