8c19e81f1e44ab1ef1f3002b1dc076b37a4b8b5d
309 <309@academicos.cesde.edu.co>
2023-08-15 08:06:13 -0500

    feat:creamos variables primitivas y lista de datos

---
+++ Ejemplo1.py
@@ -0,0 +1,12 @@
+#comentario Linea
+nombreUsuario ="Juan camilo valencia castrillon"
+edadUsuario = "26"
+estaturaUsuario = 1.80
+esHinchaDelVerde = True 
+
+print (f"Su nombre es: {nombreUsuario} y tiene {edadUsuario}años")
+
+comidasFavoritas=["frisoles","chicharron","mazamorra","carneMolida"]
+
+print(comidasFavoritas)
+print(comidasFavoritas[2])
